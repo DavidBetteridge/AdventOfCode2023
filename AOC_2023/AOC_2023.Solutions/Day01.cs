aeb3ce36fb449bb9fea40fd9e96e122edfd7679d
davidbetteridge <david.betteridge@zeromission.io>
2023-12-01 07:33:53 +0000

    refactor

--- AOC_2023/AOC_2023.Solutions/Day01.cs
+++ AOC_2023/AOC_2023.Solutions/Day01.cs
@@ -2,17 +2,8 @@
 
 public class Day01
 {
-    public int Part1(string filename)
-    {
-        var lines = File.ReadAllLines(filename);
-        var total = 0;
-        foreach (var line in lines)
-        {
-            var first = line.FirstOrDefault(char.IsDigit);
-            var last = line.LastOrDefault(char.IsDigit);
-            var number = int.Parse($"{first}{last}");
-            total += number;
-        }
-        return total;
-    }
+    public int Part1(string filename) => 
+        File.ReadAllLines(filename).Sum(line => int.Parse(line.First(char.IsDigit).ToString()) * 10 + 
+                                                int.Parse(line.Last(char.IsDigit).ToString()));
+    
 }
