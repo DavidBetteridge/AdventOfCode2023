7f5739d0dbef492d0e09802a9bf99b779eed0a2d
davidbetteridge <david.betteridge@zeromission.io>
2023-12-01 08:53:49 +0000

    Paths

--- AOC_2023/AOC_2023.Tests/Day01/Day01_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day01/Day01_Tests.cs
@@ -6,27 +6,27 @@ public class Day01Tests
     public void Test_Part1_Sample()
     {
         var solver = new Day01();
-        Assert.Equal(142, solver.Part1("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day01/part1_sample.txt"));
+        Assert.Equal(142, solver.Part1("Day01/part1_sample.txt"));
     }
     
     [Fact]
     public void Test_Part1()
     {
         var solver = new Day01();
-        Assert.Equal(56049, solver.Part1("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day01/input.txt"));
+        Assert.Equal(56049, solver.Part1("Day01/input.txt"));
     }
     
     [Fact]
     public void Test_Part2_Sample()
     {
         var solver = new Day01();
-        Assert.Equal(281, solver.Part2("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day01/part2_sample.txt"));
+        Assert.Equal(281, solver.Part2("Day01/part2_sample.txt"));
     }
     
     [Fact]
     public void Test_Part2()
     {
         var solver = new Day01();
-        Assert.Equal(54530, solver.Part2("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day01/input.txt"));
+        Assert.Equal(54530, solver.Part2("Day01/input.txt"));
     }
 }
