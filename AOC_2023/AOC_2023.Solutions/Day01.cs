d2fba0dce11a836d88fc18b6a404319920110cf8
davidbetteridge <david.betteridge@zeromission.io>
2023-12-01 08:28:29 +0000

    Use map

--- AOC_2023/AOC_2023.Solutions/Day01.cs
+++ AOC_2023/AOC_2023.Solutions/Day01.cs
@@ -2,8 +2,12 @@
 
 public class Day01
 {
+    private static readonly Dictionary<char, int> CharToInt = new()
+    {
+        ['0'] = 0, ['1'] = 1, ['2'] = 2, ['3'] = 3, ['4'] = 4, ['5'] = 5, ['6'] = 6, ['7'] = 7, ['8'] = 8, ['9'] = 9
+    };
+
     public int Part1(string filename) => 
-        File.ReadAllLines(filename).Sum(line => int.Parse(line.First(char.IsDigit).ToString()) * 10 + 
-                                                int.Parse(line.Last(char.IsDigit).ToString()));
-    
+        File.ReadAllLines(filename).Sum(line => CharToInt[line.First(char.IsDigit)] * 10 + 
+                                                CharToInt[line.Last(char.IsDigit)]);
 }
