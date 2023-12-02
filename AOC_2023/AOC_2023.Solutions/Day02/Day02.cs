8dc9bd4221ede4a134d24333c1d0c30cc967bfec
davidbetteridge <david.betteridge@zeromission.io>
2023-12-02 13:03:24 +0000

    Day 2 - Part 2

--- AOC_2023/AOC_2023.Solutions/Day02/Day02.cs
+++ AOC_2023/AOC_2023.Solutions/Day02/Day02.cs
@@ -39,4 +39,45 @@ public class Day02
 
         return result;
     }
+
+    public int Part2(string filename)
+    {
+        var result = 0;
+        var lines = File.ReadAllLines(filename);
+        foreach (var line in lines)
+        {
+            var gameId = line.Split(":")[0];
+            var id = gameId[5..];
+            var draws = line.Split(": ")[1];
+            var selections = draws.Split("; ");
+
+            var maxRed = 0;
+            var maxGreen = 0;
+            var maxBlue = 0;
+            
+            foreach (var selection in selections)
+            {
+                var colours = selection.Split(", ");
+                foreach (var colourAndQuantity in colours)
+                {
+                    var colour = colourAndQuantity.Split(" ")[1];
+                    var quantity = int.Parse(colourAndQuantity.Split(" ")[0]);
+
+                    if (colour == "red")
+                        maxRed = Math.Max(maxRed, quantity);
+
+                    if (colour == "green")
+                        maxGreen = Math.Max(maxGreen, quantity);
+                    
+                    if (colour == "blue")
+                        maxBlue = Math.Max(maxBlue, quantity);
+                    
+                }
+            }
+
+            result += (maxRed * maxGreen * maxBlue);
+        }
+
+        return result;
+    }
 }
--- AOC_2023/AOC_2023.Tests/Day02/Day02_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day02/Day02_Tests.cs
@@ -6,27 +6,27 @@ public class Day02Tests
     public void Test_Part1_Sample()
     {
         var solver = new Day02();
-        Assert.Equal(8, solver.Part1("Day02/part1_sample.txt"));
+        Assert.Equal(8, solver.Part1("Day02/sample.txt"));
     }
     
     [Fact]
     public void Test_Part1()
     {
         var solver = new Day02();
-        Assert.Equal(56049, solver.Part1("Day02/input.txt"));
+        Assert.Equal(2348, solver.Part1("Day02/input.txt"));
     }
-    //
-    // [Fact]
-    // public void Test_Part2_Sample()
-    // {
-    //     var solver = new Day01();
-    //     Assert.Equal(281, solver.Part2("Day01/part2_sample.txt"));
-    // }
-    //
-    // [Fact]
-    // public void Test_Part2()
-    // {
-    //     var solver = new Day01();
-    //     Assert.Equal(54530, solver.Part2("Day01/input.txt"));
-    // }
+    
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Day02();
+        Assert.Equal(2286, solver.Part2("Day02/sample.txt"));
+    }
+    
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Day02();
+        Assert.Equal(76008, solver.Part2("Day02/input.txt"));
+    }
 }
