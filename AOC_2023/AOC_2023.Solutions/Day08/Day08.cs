546a6b69c0a52401227527eaa29e9fde80bda72b
davidbetteridge <david.betteridge@zeromission.io>
2023-12-08 20:15:20 +0000

    Remove parser

--- AOC_2023/AOC_2023.Solutions/Day08/Day08.cs
+++ AOC_2023/AOC_2023.Solutions/Day08/Day08.cs
@@ -4,6 +4,7 @@ public class Day08
 {
     private struct Rule
     {
+        public string Start { get; set; }
         public string LHS { get; set; }
         public string RHS { get; set; }
     }
@@ -45,50 +46,61 @@ public class Day08
 
         return moves;
     }
-
+    
+    
     public long Part2(string filename)
     {
         var lines = File.ReadAllLines(filename);
-        var lrParser = new LRParserSpan();
-
+        var length = lines.Length - 2;
         var instructions = lines[0];
-        var rules = new Dictionary<string, Rule>();
-        foreach (var line in lines[2..])
+        var rules = new Rule[length];
+        var mapping = new Dictionary<string, int>(length);
+
+        for (var i = 0; i < length; i++)
         {
-            lrParser.Reset(line);
-            var start = lrParser.EatWord();
-            lrParser.EatWhitespace();
-            lrParser.Eat('=');
-            lrParser.EatWhitespace();
-            lrParser.Eat('(');
-            var left = lrParser.EatWord();
-            lrParser.Eat(',');
-            lrParser.EatWhitespace();
-            var right = lrParser.EatWord();
-            lrParser.Eat(')');
-            rules.Add(start, new Rule { LHS = left, RHS = right });
+            var line = lines[i + 2];
+            var start = line[..3];
+            var left = line[7..10];
+            var right = line[12..15];
+            rules[i] = new Rule { Start = start, LHS = left, RHS = right };
+            mapping.Add(start, i);
+        }
+        
+        var rulesLhs = new int[length];
+        var rulesRhs = new int[length];
+        var starts = new bool[length];
+        var ends = new bool[length];
+        foreach (var ghost in rules)
+        {
+            var i = mapping[ghost.Start];
+            starts[i] = ghost.Start.EndsWith("A");
+            ends[i] = ghost.Start.EndsWith("Z");
+            rulesLhs[i] = mapping[ghost.LHS];
+            rulesRhs[i] = mapping[ghost.RHS];
         }
         
         var y = 0L;
-        foreach (var ghost in rules.Keys.Where(r => r.EndsWith('A')))
+        for (var ghost = 0; ghost < length; ghost++)
         {
-            var currentLocation = ghost;
-            var offset = 0;
-
-            // Move each ghost until it's on a terminal
-            while (!currentLocation.EndsWith('Z'))
+            if (starts[ghost])
             {
-                var nextMove = instructions[offset % instructions.Length];
-                if (nextMove == 'L')
-                    currentLocation = rules[currentLocation].LHS;
-                else
-                    currentLocation = rules[currentLocation].RHS;
-                offset++;
+                var currentLocation = ghost;
+                var offset = 0;
+
+                // Move each ghost until it's on a terminal
+                while (!ends[currentLocation])
+                {
+                    var nextMove = instructions[offset % instructions.Length];
+                    if (nextMove == 'L')
+                        currentLocation = rulesLhs[currentLocation];
+                    else
+                        currentLocation = rulesRhs[currentLocation];
+                    offset++;
+                }
+
+                y = y == 0 ? offset : Lcm(y, offset);
             }
-
-            y = y == 0 ? offset : Lcm(y, offset);
         }
-
         return y;
     }
 
