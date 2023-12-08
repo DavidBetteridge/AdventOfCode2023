d6ccff3105757fa1e4b64ce14834f1d82152e8e5
davidbetteridge <david.betteridge@zeromission.io>
2023-12-08 09:30:56 +0000

    Day 8 - Part 2 - Slow

--- AOC_2023/AOC_2023.Tests/Day08/Day08_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day08/Day08_Tests.cs
@@ -23,17 +23,17 @@ public class Day08Tests
         Assert.Equal(19099, solver.Part1("Day08/input.txt"));
     }
     
-    // [Fact]
-    // public void Test_Part2_Sample()
-    // {
-    //     var solver = new Day08();
-    //     Assert.Equal(5905, solver.Part2("Day08/sample.txt"));
-    // }
-    //
-    // [Fact]
-    // public void Test_Part2()
-    // {
-    //     var solver = new Day08();
-    //     Assert.Equal(250825981, solver.Part2("Day08/input.txt"));
-    // }
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Day08();
+        Assert.Equal((ulong)6, solver.Part2("Day08/sample3.txt"));
+    }
+    
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Day08();
+        Assert.Equal((ulong)17099847107071, solver.Part2("Day08/input.txt"));
+    }
 }
