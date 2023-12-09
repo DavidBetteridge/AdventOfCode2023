a8a532a275a9bdd1a6731cffa9cd11a2bae42ecf
davidbetteridge <david.betteridge@zeromission.io>
2023-12-09 23:31:02 +0000

    Day10 ready

---
+++ AOC_2023/AOC_2023.Solutions/Day10/Day10.cs
@@ -0,0 +1,11 @@
+namespace AOC_2023.Solutions;
+
+public class Day10
+{
+    public int Part1(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var lrParser = new LRParserSpan();
+        return 0;
+    }
+}
