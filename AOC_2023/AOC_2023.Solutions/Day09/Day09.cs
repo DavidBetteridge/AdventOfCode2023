a14e12e49e2b38093b98f4ee50b0630dc8babf24
davidbetteridge <david.betteridge@zeromission.io>
2023-12-09 14:53:56 +0000

    Day 9 Part 2

--- AOC_2023/AOC_2023.Solutions/Day09/Day09.cs
+++ AOC_2023/AOC_2023.Solutions/Day09/Day09.cs
@@ -42,4 +42,45 @@ public class Day09
         
         return total;
     }
+    
+    public int Part2(string filename)
+    {
+        var lines = File.ReadAllLines(filename);
+        var lrParser = new LRParserSpan();
+        var total = 0;
+        foreach (var line in lines)
+        {
+            // Level 0 contains the original numbers.
+            var ns = new List<List<int>>() { new ()};
+            lrParser.Reset(line);
+            do
+            {
+                ns[0].Add(lrParser.EatNumber());
+                lrParser.EatWhitespace();
+            } while (!lrParser.EOF);
+            
+            // Work out other levels until we get to all zeros
+            var allZeros = false;
+            var level = 0;
+            while (!allZeros)
+            {
+                allZeros = true;
+                ns.Add(new List<int>());
+                for (var i = 0; i < ns[level].Count - 1; i++)
+                {
+                    var diff = ns[level][i + 1] - ns[level][i];
+                    ns[level+1].Add(ns[level][i+1]-ns[level][i]);
+                    if (diff != 0) allZeros = false;
+                }
+                level++;
+            }
+
+            ns[level].Add(0);
+            for (level = ns.Count - 2; level >= 0; level--)
+                ns[level].Add(ns[level][0]-ns[level+1][^1]);
+            total += ns[0][^1];
+        }
+        
+        return total;
+    }
 }
--- AOC_2023/AOC_2023.Tests/Day09/Day09_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day09/Day09_Tests.cs
@@ -16,17 +16,17 @@ public class Day09Tests
         Assert.Equal(19099, solver.Part1("Day09/input.txt"));
     }
     
-    // [Fact]
-    // public void Test_Part2_Sample()
-    // {
-    //     var solver = new Day08Generic();
-    //     Assert.Equal((ulong)6, solver.Part2("Day08/sample3.txt"));
-    // }
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Day09();
+        Assert.Equal(2, solver.Part2("Day09/sample.txt"));
+    }
     
-    // [Fact]
-    // public void Test_Part2()
-    // {
-    //     var solver = new Day08Generic();
-    //     Assert.Equal((ulong)17099847107071, solver.Part2("Day08/input.txt"));
-    // }
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Day09();
+        Assert.Equal(7099847107071, solver.Part2("Day09/input.txt"));
+    }
 }
