ae1421ac8dc564bca49fb18bb7075448b62994f4
davidbetteridge <david.betteridge@zeromission.io>
2023-12-09 23:28:41 +0000

    Day9

--- AOC_2023/AOC_2023.Tests/Day09/Day09_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day09/Day09_Tests.cs
@@ -13,7 +13,7 @@ public class Day09Tests
     public void Test_Part1()
     {
         var solver = new Day09();
-        Assert.Equal(19099, solver.Part1("Day09/input.txt"));
+        Assert.Equal(1938731307, solver.Part1("Day09/input.txt"));
     }
     
     [Fact]
@@ -27,6 +27,6 @@ public class Day09Tests
     public void Test_Part2()
     {
         var solver = new Day09();
-        Assert.Equal(7099847107071, solver.Part2("Day09/input.txt"));
+        Assert.Equal(948, solver.Part2("Day09/input.txt"));
     }
 }
