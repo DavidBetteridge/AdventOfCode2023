c7338e31069621da0dde854bb8bb5765f9610e3c
davidbetteridge <david.betteridge@zeromission.io>
2023-12-13 10:08:09 +0000

    Day 13 - Part 2

--- AOC_2023/AOC_2023.Tests/Day13/Day13_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day13/Day13_Tests.cs
@@ -16,17 +16,17 @@ public class Day13Tests
         Assert.Equal(33780, solver.Part1("Day13/input.txt"));
     }
     
-    // [Fact]
-    // public void Test_Part2_Sample()
-    // {
-    //     var solver = new Day13();
-    //     Assert.Equal(525152, solver.Part1And2("Day12/sample.txt", 5));
-    // }
-    //
-    // [Fact]
-    // public void Test_Part2()
-    // {
-    //     var solver = new Day12();
-    //     Assert.Equal(25470469710341, solver.Part1And2("Day12/input.txt", 5));
-    // }
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Day13Part2();
+        Assert.Equal(400, solver.Part2("Day13/sample.txt"));
+    }
+
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Day13Part2();
+        Assert.Equal(23479, solver.Part2("Day13/input.txt"));
+    }
 }
