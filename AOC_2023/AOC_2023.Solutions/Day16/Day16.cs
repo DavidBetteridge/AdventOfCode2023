1f202e175254118df57861a6bc8e645f55d63cba
davidbetteridge <david.betteridge@zeromission.io>
2023-12-16 15:39:38 +0000

    Faster...

--- AOC_2023/AOC_2023.Solutions/Day16/Day16.cs
+++ AOC_2023/AOC_2023.Solutions/Day16/Day16.cs
@@ -2,41 +2,81 @@ namespace AOC_2023.Solutions;
 
 public class Day16
 {
+    private const byte Up = 0b00;
+    private const byte Down = 0b01;
+    private const byte Left = 0b10;
+    private const byte Right = 0b11;
+    
+    private const byte Dot = 0b000;
+    private const byte Vertical = 0b001;
+    private const byte Horizontal = 0b010;
+    private const byte Slash = 0b011;  // /
+    private const byte BackSlash = 0b100;  // \
+    
     public long Part1(string filename)
     {
-        var cave = File.ReadAllLines(filename);
-        return Solve(cave, 0, 0, 'R');
+        var cave = File.ReadAllLines(filename)
+            .Select(line => line.Select(c => c switch
+                        {
+                            '-' => Horizontal,
+                            '|' => Vertical,
+                            '/' => Slash,
+                            '\\' => BackSlash,
+                            _ => Dot
+                        }            
+                        ).ToArray()).ToArray();
+        
+        var visited = new HashSet<(int x, int y)>();
+        var repeats = new HashSet<(int x, int y ,byte dir)>();
+        var beams = new Queue<(int x, int y ,byte dir)>();
+        return Solve(cave, 0, 0, Right, visited, repeats, beams);
     }
 
     public long Part2(string filename)
     {
-        var cave = File.ReadAllLines(filename);
+        var cave = File.ReadAllLines(filename)
+            .Select(line => line.Select(c => c switch
+                {
+                    '-' => Horizontal,
+                    '|' => Vertical,
+                    '/' => Slash,
+                    '\\' => BackSlash,
+                    _ => Dot
+                }            
+            ).ToArray()).ToArray();
         var rows = cave.Length;
         var cols = cave[0].Length;
         var result = 0;
+        var visited = new HashSet<(int x, int y)>();
+        var repeats = new HashSet<(int x, int y ,byte dir)>();
+        var beams = new Queue<(int x, int y ,byte dir)>();
         
         for (var x = 0; x < cols; x++)
         {
-            result = Math.Max(result, Solve(cave, x, 0, 'D'));
-            result = Math.Max(result, Solve(cave, x, rows-1, 'U'));
+            result = Math.Max(result, Solve(cave, x, 0, Down, visited, repeats, beams));
+            result = Math.Max(result, Solve(cave, x, rows-1, Up, visited, repeats, beams));
         }
         
         for (var y = 0; y < rows; y++)
         {
-            result = Math.Max(result, Solve(cave, 0, y, 'R'));
-            result = Math.Max(result, Solve(cave, cols-1, y, 'L'));
+            result = Math.Max(result, Solve(cave, 0, y, Right, visited, repeats, beams));
+            result = Math.Max(result, Solve(cave, cols-1, y, Left, visited, repeats, beams));
         }
         
         return result;
     }
     
-    private int Solve(string[] cave, int startX, int startY, char initialDir)
+    private int Solve(byte[][] cave, int startX, int startY, byte initialDir,
+                      HashSet<(int x, int y)> visited, 
+                      HashSet<(int x, int y, byte dir)> repeats, 
+                      Queue<(int x, int y, byte dir)> beams)
     {
+        visited.Clear();
+        repeats.Clear();
+        
         var rows = cave.Length;
         var cols = cave[0].Length;
-        var visited = new HashSet<(int x, int y)>();
-        var repeats = new HashSet<(int x, int y ,char dir)>();
-        var beams = new Queue<(int x, int y ,char dir)>();
+
         beams.Enqueue((startX, startY, initialDir));
 
         while (beams.Count>0)
@@ -56,87 +96,75 @@ public class Day16
             // What is this cell?
             var c = cave[newY][newX];
             
-            if (dir is 'R' or 'L' && c == '|')
+            if (dir is Right or Left && c == Vertical)
             {
                 // Split
-                beams.Enqueue((newX, newY - 1, 'U'));
-                beams.Enqueue((newX, newY + 1, 'D'));
+                beams.Enqueue((newX, newY - 1, Up));
+                beams.Enqueue((newX, newY + 1, Down));
                 continue;
             }
 
-            if (dir is 'U' or 'D' && c == '-')
+            if (dir is Up or Down && c == Horizontal)
             {
                 // Split
-                beams.Enqueue((newX - 1, newY, 'L'));
-                beams.Enqueue((newX + 1, newY, 'R'));
+                beams.Enqueue((newX - 1, newY, Left));
+                beams.Enqueue((newX + 1, newY, Right));
                 continue;
             }
 
-            if (dir == 'R' && c == '/')
+            if (dir == Right && c == Slash)
             {
-                beams.Enqueue((newX, newY - 1, 'U'));
+                beams.Enqueue((newX, newY - 1, Up));
                 continue;
             }
 
-            if (dir == 'D' && c == '/')
+            if (dir == Down && c == Slash)
             {
-                beams.Enqueue((newX - 1, newY, 'L'));
+                beams.Enqueue((newX - 1, newY, Left));
                 continue;
             }
 
-            if (dir == 'L' && c == '/')
+            if (dir == Left && c == Slash)
             {
-                beams.Enqueue((newX, newY + 1, 'D'));
+                beams.Enqueue((newX, newY + 1, Down));
                 continue;
             }
 
-            if (dir == 'U' && c == '/')
+            if (dir == Up && c == Slash)
             {
-                beams.Enqueue((newX + 1, newY, 'R'));
+                beams.Enqueue((newX + 1, newY, Right));
                 continue;
             }
 
-            if (dir == 'R' && c == '\\')
+            if (dir == Right && c == BackSlash)
             {
-                beams.Enqueue((newX, newY + 1, 'D'));
+                beams.Enqueue((newX, newY + 1, Down));
                 continue;
             }
 
-            if (dir == 'D' && c == '\\')
+            if (dir == Down && c == BackSlash)
             {
-                beams.Enqueue((newX + 1, newY, 'R'));
+                beams.Enqueue((newX + 1, newY, Right));
                 continue;
             }
 
-            if (dir == 'L' && c == '\\')
+            if (dir == Left && c == BackSlash)
             {
-                beams.Enqueue((newX, newY - 1, 'U'));
+                beams.Enqueue((newX, newY - 1, Up));
                 continue;
             }
 
-            if (dir == 'U' && c == '\\')
+            if (dir == Up && c == BackSlash)
             {
-                beams.Enqueue((newX - 1, newY, 'L'));
+                beams.Enqueue((newX - 1, newY, Left));
                 continue;
             }
             
             // Keep walking
-            if (dir == 'L') beams.Enqueue((newX - 1, newY, dir));
-            if (dir == 'R') beams.Enqueue((newX + 1, newY, dir));
-            if (dir == 'U') beams.Enqueue((newX, newY - 1, dir));
-            if (dir == 'D') beams.Enqueue((newX, newY + 1, dir));
-        }
-
-        for (var row = 0; row < rows; row++)
-        {
-            for (var col = 0; col < cols; col++)
-            {   
-                if (visited.Contains((col, row)))
-                    Console.Write('#');
-                else
-                    Console.Write('.');
-            }
-            Console.WriteLine();
+            if (dir == Left) beams.Enqueue((newX - 1, newY, dir));
+            if (dir == Right) beams.Enqueue((newX + 1, newY, dir));
+            if (dir == Up) beams.Enqueue((newX, newY - 1, dir));
+            if (dir == Down) beams.Enqueue((newX, newY + 1, dir));
         }
        
         return visited.Count;
--- AOC_2023/AOC_2023.Solutions/Day16/Day16BenchmarkTests.cs
+++ AOC_2023/AOC_2023.Solutions/Day16/Day16BenchmarkTests.cs
@@ -11,11 +11,11 @@ public class Day16BenchmarkTests
         var solver = new Day16();
         solver.Part1("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day16/input.txt");
     }
-    //
-    // [Benchmark(Baseline = false)]
-    // public void Part2()
-    // {
-    //     var solver = new Day16();
-    //     solver.Part2("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day16/input.txt");
-    // }
+    
+    [Benchmark(Baseline = false)]
+    public void Part2()
+    {
+        var solver = new Day16();
+        solver.Part2("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day16/input.txt");
+    }
 }
