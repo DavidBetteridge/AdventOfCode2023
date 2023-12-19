263ef59940cfeed79c27f82c67c9e37d68300210
davidbetteridge <david.betteridge@zeromission.io>
2023-12-19 23:48:35 +0000

    Part 2

--- AOC_2023/AOC_2023.Solutions/Day17/Day17.cs
+++ AOC_2023/AOC_2023.Solutions/Day17/Day17.cs
@@ -7,7 +7,18 @@ public class Day17
         public int Node { get; set; }
         public int Weight { get; set; }
     }
+
     public long Part1(string filename)
+    {
+        return Solve(filename, 1, 3);
+    }
+    
+    public long Part2(string filename)
+    {
+        return Solve(filename, 4, 10);
+    }
+
+    private long Solve(string filename, int min, int max)
     {
         var weights = File.ReadAllLines(filename)
                             .Select(line => line.Select(c => c - '0').ToArray())
@@ -16,74 +27,90 @@ public class Day17
         var numberOfRows = weights.Length;
         var numberOfColumns = weights[0].Length;
         var numberOfCells = numberOfColumns * numberOfRows;
+        var range = max - min + 1;
         
         // Horizontal nodes are.....  col + row * number of cols
         // Vertical nodes are ..... col  + (row * number of cols) + (number of rows * number of cols)
         // but we are really interested in the links between nodes.
         // Mapping Source -> Targets x 6 (Node, Weight)
         var numberOfNodes = (numberOfCells * 2);
-        var graph = new Link?[numberOfNodes, 6];
+        var graph = new Link?[numberOfNodes, range*2];  // Range in either direction
         
         for (var row = 0; row < numberOfRows; row++)
         {
             for (var column = 0; column < numberOfColumns; column++)
             {
+                // Target cell has no links out.
                 if ((row + 1 == numberOfRows) && (column + 1 == numberOfColumns))
                     continue;
                 
                 // Horizontal node
                 var sourceH = column + (row * numberOfColumns);
+                var index = 0;
                 
                 // Going right....
                 var weight = 0;
-                for (var length = 1; length <= 3; length++)
+                for (var length = 1; length <= max; length++)
                 {
                     if (column + 1 + length <= numberOfColumns)
                     {
-                        var target = (column + length) + (row * numberOfColumns) + numberOfCells;
-                        if (target == numberOfNodes - 1) target = numberOfCells - 1;
                         weight += weights[row][column + length];
-                        graph[sourceH, length - 1] = new Link { Node = target, Weight = weight };
+                        if (length >= min)
+                        {
+                            var target = (column + length) + (row * numberOfColumns) + numberOfCells;
+                            if (target == numberOfNodes - 1) target = numberOfCells - 1;
+                            graph[sourceH, index++] = new Link { Node = target, Weight = weight };
+                        }
                     }
                 }
                 
                 // Going left....
                 weight = 0;
-                for (var length = 1; length <= 3; length++)
+                for (var length = 1; length <= max; length++)
                 {
                     if (column - length >= 0)
                     {
-                        var target = (column-length) + (row * numberOfColumns) + numberOfCells;
                         weight += weights[row][column - length];
-                        graph[sourceH, 3 + length - 1] = new Link { Node = target, Weight = weight };
+                        if (length >= min)
+                        {
+                            var target = (column - length) + (row * numberOfColumns) + numberOfCells;
+                            graph[sourceH, index++] = new Link { Node = target, Weight = weight };
+                        }
                     }
                 }
                 
                 // Vertical node
                 var sourceV = column + (row * numberOfColumns) + numberOfCells;
+                index = 0;
                 
                 // Going down....
                 weight = 0;
-                for (var length = 1; length <= 3; length++)
+                for (var length = 1; length <= max; length++)
                 {
                     if (row + 1 + length <= numberOfRows)
                     {
-                        var target = column + ((row + length)  * numberOfColumns);
-                        if (target == numberOfNodes - 1) target = numberOfCells - 1;
                         weight += weights[row + length][column];
-                        graph[sourceV, length - 1] = new Link { Node = target, Weight = weight };
+                        if (length >= min)
+                        {
+                            var target = column + ((row + length) * numberOfColumns);
+                            if (target == numberOfNodes - 1) target = numberOfCells - 1;
+                            graph[sourceV, index++] = new Link { Node = target, Weight = weight };
+                        }
                     }
                 }
                 
                 // Going up....
                 weight = 0;
-                for (var length = 1; length <= 3; length++)
+                for (var length = 1; length <= max; length++)
                 {
                     if (row - length >= 0)
                     {
-                        var target = column + ((row - length)  * numberOfColumns);
                         weight += weights[row - length][column];
-                        graph[sourceV, 3 + length - 1] = new Link { Node = target, Weight = weight };
+                        if (length >= min)
+                        {
+                            var target = column + ((row - length) * numberOfColumns);
+                            graph[sourceV, index++] = new Link { Node = target, Weight = weight };
+                        }
                     }
                 }
             }
@@ -91,13 +118,17 @@ public class Day17
 
         // bit hacky,  but we can also go down from the first node
         var w = 0;
-        for (var length = 1; length <= 3; length++)
+        var j = 0;
+        for (var length = 1; length <= max; length++)
         {
             if (1 + length <= numberOfRows)
             {
-                var target = 0 + (length * numberOfColumns);
                 w += weights[length][0];
-                graph[0, 3+length - 1] = new Link { Node = target, Weight = w };
+                if (length >= min)
+                {
+                    var target = 0 + (length * numberOfColumns);
+                    graph[0, range + (j++)] = new Link { Node = target, Weight = w };
+                }
             }
         }
         
@@ -113,10 +144,10 @@ public class Day17
             var v = FindSmallest(seen, distances);
             seen[v] = true;
 
-            for (var u = 0; u < 6; u++)
+            for (var u = 0; u < (range*2); u++)
             {
                 var link = graph[v, u];
-                if (link is not null)
+                if (link is not null && distances[v] != int.MaxValue)
                 {
                     if ((distances[v] + link.Weight) < distances[link.Node])
                     {
@@ -132,7 +163,7 @@ public class Day17
 
     private int FindSmallest(bool[] seen, int[] distances)
     {
-        var smallestCost = int.MaxValue-1;
+        var smallestCost = int.MaxValue;
         var smallestIndex = -1;
 
         for (var i = 0; i < distances.Length; i++)
@@ -146,4 +177,6 @@ public class Day17
 
         return smallestIndex;
     }
+
+
 }
--- AOC_2023/AOC_2023.Tests/Day17/Day17_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day17/Day17_Tests.cs
@@ -13,20 +13,27 @@ public class Day17Tests
     public void Test_Part1()
     {
         var solver = new Day17();
-        Assert.Equal(7111, solver.Part1("Day17/input.txt"));
+        Assert.Equal(755, solver.Part1("Day17/input.txt"));
     }
 
-    // [Fact]
-    // public void Test_Part2_Sample()
-    // {
-    //     var solver = new Day16();
-    //     Assert.Equal(51, solver.Part2("Day16/sample.txt"));
-    // }
-    //
-    // [Fact]
-    // public void Test_Part2()
-    // {
-    //     var solver = new Day16();
-    //     Assert.Equal(7831, solver.Part2("Day16/input.txt"));
-    // }
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Day17();
+        Assert.Equal(94, solver.Part2("Day17/sample.txt"));
+    }
+    
+    [Fact]
+    public void Test_Part2_Sample2()
+    {
+        var solver = new Day17();
+        Assert.Equal(71, solver.Part2("Day17/sample3.txt"));
+    }
+    
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Day17();
+        Assert.Equal(881, solver.Part2("Day17/input.txt"));  //881 too low
+    }
 }
