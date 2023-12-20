8bb0fdf5f2587cd4f7b1a17935a3cd3d7defdd51
davidbetteridge <david.betteridge@zeromission.io>
2023-12-20 23:35:50 +0000

    Refactor

--- AOC_2023/AOC_2023.Solutions/Day18/Day18.cs
+++ AOC_2023/AOC_2023.Solutions/Day18/Day18.cs
@@ -13,15 +13,14 @@ public class Day18
         var commands = File.ReadAllLines(filename);
         var points = new List<Point>();
         var current = new Point();
-        points.Add(current);
 
-        var result = 1;
+        long boundary = 0;
         foreach (var command in commands)
         {
             var bits = command.Split(' ');
             var dir = bits[0][0];
             var qty = int.Parse(bits[1]);
-
+            boundary += qty;
             current = dir switch
             {
                 'R' => current with { X = current.X + qty },
@@ -31,17 +30,42 @@ public class Day18
                 _ => current
             };
             points.Add(current);
-
-            // Offset because we need to take the border into account.
-            // Can also use Picks for this.
-            result += dir switch
-            {
-                'L' => qty,
-                'U' => qty,
-                _ => 0
-            };
         }
 
+        var interior = Shoelace(points);
+        return PicksTheorem(boundary, interior);
+    }
+
+    public long Part2(string filename)
+    {
+        var commands = File.ReadAllLines(filename);
+        var points = new List<Point>();
+        var current = new Point();
+
+        long boundary = 0;
+        foreach (var command in commands)
+        {
+            // R 6 (#70c710)
+            var bits = command.Split(' ')[^1]; // (#70c710)
+            var dirC = bits[7];
+            var qty = Convert.ToInt32($"0x{bits[2..7]}", 16);
+            boundary += qty;
+            current = dirC switch
+            {
+                '0' => current with { X = current.X + qty },
+                '1' => current with { Y = current.Y - qty },
+                '2' => current with { X = current.X - qty },
+                _ => current with { Y = current.Y + qty },
+            };
+            points.Add(current);
+        }
+
+        var interior = Shoelace(points);
+        return PicksTheorem(boundary, interior);
+    }
+    
+    private static long Shoelace(List<Point> points)
+    {
         var N = points.Count - 1;
         var r1 = points[N].X * points[0].Y;
         for (var n = 0; n < N; n++)
@@ -51,60 +75,11 @@ public class Day18
         for (var n = 0; n < N; n++)
             r2 += points[n + 1].X * points[n].Y;
 
-        return result + (Math.Abs(r1 - r2) / 2);
+        return (Math.Abs(r1 - r2) / 2);
     }
 
-    public double Part2(string filename)
+    private static long PicksTheorem(long boundary, long interior)
     {
-        var commands = File.ReadAllLines(filename);
-        var points = new List<Point>();
-        var current = new Point();
-        points.Add(current);
-
-        double result = 1L;
-        foreach (var command in commands)
-        {
-            // R 6 (#70c710)
-            var bits = command.Split(' ')[^1]; // (#70c710)
-            var dirC = bits[7];
-            var qty = Convert.ToInt32($"0x{bits[2..7]}", 16);
-
-            var dir = dirC switch
-            {
-                '0' => 'R',
-                '1' => 'D',
-                '2' => 'L',
-                _ => 'U'
-            };
-
-            current = dir switch
-            {
-                'R' => current with { X = current.X + qty },
-                'L' => current with { X = current.X - qty },
-                'D' => current with { Y = current.Y + qty },
-                'U' => current with { Y = current.Y - qty },
-                _ => current
-            };
-            points.Add(current);
-
-            // Offset because we need to take the border into account.
-            // Can also use Picks for this.
-            result += dir switch
-            {
-                'L' => qty,
-                'U' => qty,
-                _ => 0
-            };
-        }
-
-        int n = points.Count - 1;
-
-        double a = 0;
-        for (int i = 0; i < n; i++)
-        {
-            a += ((points[i].X * points[i + 1].Y) - (points[i + 1].X * points[i].Y)) / 2.0;
-        }
-
-        return result + (a);
+        return interior + (boundary / 2) + 1;   // Had to swap - for +
     }
 }
