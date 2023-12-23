f1e81b0a5984f971ebb56c9d73198a8f12457d6e
davidbetteridge <david.betteridge@zeromission.io>
2023-12-23 22:30:03 +0000

    Day 23 Part 2

--- AOC_2023/AOC_2023.Solutions/Day23/Day23.cs
+++ AOC_2023/AOC_2023.Solutions/Day23/Day23.cs
@@ -63,5 +63,53 @@ public class Day23
         
     }
 
+    public long Part2(string filename)
+    {
+        var wood = File.ReadAllLines(filename);
+        var rows = wood.Length;
+        var cols = wood[0].Length;
+        
+        var startX = wood[0].IndexOf('.');
+        var startY = 0;
+        
+        var endX = wood[^1].IndexOf('.');
+        var endY = wood.Length-1;
 
+        var seen = new bool[rows, cols];
+        
+        return Walk(startX, startY, 0);
+        
+        int Walk(int x, int y, int numberOfSteps)
+        {
+            var result = 0;
+            
+            if (x < 0 || x >= cols) return -4;
+            if (y < 0 || y >= rows) return -5;
+            if (wood[y][x] == '#') return -3;
+            
+            if (x == endX && y == endY)
+            {
+                return numberOfSteps;
+            }
+
+            // Console.WriteLine($"{x}, {y} {numberOfSteps}");
+            
+            if (seen[y, x]) 
+                return -2;
+
+            seen[y, x] = true;
+   
+            // Try all directions
+            result = Walk(x + 1, y, numberOfSteps + 1);
+            result = Math.Max(result, Walk(x - 1, y, numberOfSteps + 1));
+            result = Math.Max(result, Walk(x, y+1, numberOfSteps + 1));
+            result = Math.Max(result, Walk(x, y-1, numberOfSteps + 1));
+
+            
+            seen[y, x] = false;
+            
+            return result;
+        }
+        
+    }
 }
--- AOC_2023/AOC_2023.Tests/Day23/Day23_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day23/Day23_Tests.cs
@@ -13,20 +13,20 @@ public class Day23Tests
     public void Test_Part1()
     {
         var solver = new Day23();
-        Assert.Equal(3605, solver.Part1("Day23/input.txt"));
+        Assert.Equal(2442, solver.Part1("Day23/input.txt"));
     }
-    //
-    // [Fact]
-    // public void Test_Part2_Sample()
-    // {
-    //     var solver = new Day23();
-    //     Assert.Equal(167409079868000, solver.Part2("Day23/sample.txt"));
-    // }
-    //
-    // [Fact]
-    // public void Test_Part2()
-    // {
-    //     var solver = new Day23();
-    //     Assert.Equal(130090458884662, solver.Part2("Day23/input.txt"));
-    // }
+    
+    [Fact]
+    public void Test_Part2_Sample()
+    {
+        var solver = new Day23();
+        Assert.Equal(154, solver.Part2("Day23/sample.txt"));
+    }
+    
+    [Fact]
+    public void Test_Part2()
+    {
+        var solver = new Day23();
+        Assert.Equal(6898, solver.Part2("Day23/input.txt"));
+    }
 }
