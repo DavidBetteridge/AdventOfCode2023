f12cc2da2dfb60e22287802a8b60fb6343f9816f
davidbetteridge <david.betteridge@zeromission.io>
2024-01-05 22:39:39 +0000

    Start of 21 part 2

--- AOC_2023/AOC_2023.Solutions/Day21/Day21.cs
+++ AOC_2023/AOC_2023.Solutions/Day21/Day21.cs
@@ -18,16 +18,15 @@ public class Day21
             if (startCol != -1) break;
             startRow++;
         }
-        
+
         // Compute shortest paths from S
-        var distances = new int[rows*cols];
+        var distances = new int[rows * cols];
         Array.Fill(distances, int.MaxValue);
-        distances[startCol + (startRow * cols)  ] = 0;
+        distances[startCol + (startRow * cols)] = 0;
 
-        var seen = new bool[rows*cols];
+        var seen = new bool[rows * cols];
 
-       
-        for (var n = 0; n < rows*cols; n++)
+        for (var n = 0; n < rows * cols; n++)
         {
             var v = FindSmallest(seen, distances);
             if (distances[v] > requiredSteps) break;
@@ -72,11 +71,11 @@ public class Day21
                 }
             }
         }
-        
-        
+
+
         return distances.Count(d => d % 2 == 0);
     }
-    
+
     private int FindSmallest(bool[] seen, int[] distances)
     {
         var smallestCost = int.MaxValue;
@@ -93,10 +92,57 @@ public class Day21
 
         return smallestIndex;
     }
-    
-    public long Part2(string filename)
+
+
+    public long Part2(string filename, int requiredSteps)
     {
-        return 0;
+        // The garden is 11 x 100
+        // 11 is also a factor of 26501365   (2409215)
+
+        // Load Graph
+        var graph = File.ReadAllLines(filename);
+        var rows = graph.Length;
+        var cols = graph[0].Length;
+
+        // Find the start position
+        var startRow = 0;
+        var startCol = 0;
+        foreach (var row in graph)
+        {
+            startCol = row.IndexOf('S');
+            if (startCol != -1) break;
+            startRow++;
+        }
+
+        var visited = new HashSet<(int col, int row)>();
+        var queue = new Queue<(int col, int row, int steps)>();
+        queue.Enqueue((startCol, startRow, 0));
+
+        while (queue.Any())
+        {
+            var (x, y, stepsTaken) = queue.Dequeue();
+            if (stepsTaken == requiredSteps)
+            {
+                visited.Add((x, y));
+            }
+            else
+            {
+                if (graph[nmod(y,rows)][nmod(x - 1, cols)] != '#')
+                    queue.Enqueue((x - 1, y, stepsTaken + 1));
+                if (graph[nmod(y-1,rows)][nmod(x,cols)] != '#')
+                    queue.Enqueue((x, y - 1, stepsTaken + 1));
+                if (graph[nmod(y,rows)][nmod(x+1,cols)] != '#')
+                    queue.Enqueue((x+1, y, stepsTaken + 1));
+                if (graph[nmod(y + 1,rows)][nmod(x,cols)] != '#')
+                    queue.Enqueue((x, y+1, stepsTaken + 1));
+            }
+        }
+
+        return visited.Count;
     }
     
+    int nmod(int x, int m) {
+        var r = x%m;
+        return r<0 ? r+m : r;
+    }
 }
--- AOC_2023/AOC_2023.Tests/Day21/Day21_Tests.cs
+++ AOC_2023/AOC_2023.Tests/Day21/Day21_Tests.cs
@@ -13,20 +13,31 @@ public class Day21Tests
     public void Test_Part1()
     {
         var solver = new Day21();
-        Assert.Equal(3605, solver.Part1("Day21/input.txt", 64));   // Not 40
+        Assert.Equal(3605, solver.Part1("Day21/input.txt", 64));
     }
 
-    [Fact]
-    public void Test_Part2_Sample()
+    [Theory]
+    [InlineData(6, 16)]
+    [InlineData(10, 50)]
+  //  [InlineData(50, 1594)]
+    public void Test_Part2_Sample(int steps, int expected)
     {
         var solver = new Day21();
-        Assert.Equal(167409079868000, solver.Part2("Day21/sample.txt"));
+        Assert.Equal(expected, solver.Part2("Day21/sample.txt", steps));
     }
     
-    [Fact]
-    public void Test_Part2()
-    {
-        var solver = new Day21();
-        Assert.Equal(130090458884662, solver.Part2("Day21/input.txt"));
-    }
+    // In exactly 6 steps, he can still reach 16 garden plots.
+    //     In exactly 10 steps, he can reach any of 50 garden plots.
+    //     In exactly 50 steps, he can reach 1594 garden plots.
+    //     In exactly 100 steps, he can reach 6536 garden plots.
+    //     In exactly 500 steps, he can reach 167004 garden plots.
+    //     In exactly 1000 steps, he can reach 668697 garden plots.
+    //     In exactly 5000 steps, he can reach 16733044 garden plots.
+    
+    // [Fact]
+    // public void Test_Part2()
+    // {
+    //     var solver = new Day21();
+    //     Assert.Equal(130090458884662, solver.Part2("Day21/input.txt"));
+    // }
 }
