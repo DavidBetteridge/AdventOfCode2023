7cfca7de044a19f1c90e4429f74b2fa71c8cca68
davidbetteridge <david.betteridge@zeromission.io>
2024-01-11 20:54:51 +0000

    Day 03 Complete

--- AOC_2023/AOC_2023.Solutions/Day03/Day03BenchmarkTests.cs
+++ AOC_2023/AOC_2023.Solutions/Day03/Day03BenchmarkTests.cs
@@ -5,18 +5,17 @@ namespace AOC_2023.Solutions;
 [MemoryDiagnoser(true)]
 public class Day03BenchmarkTests
 {
-    
     [Benchmark]
-    public void FromStar_Part2()
+    public void Day03_Part1()
     {
         var solver = new Day03();
-        solver.Part2("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day03/input.txt");
+        solver.Part1("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day03/input.txt");
     }
     
     [Benchmark]
-    public void FromNumber_Part2()
+    public void Day03_Part2()
     {
-        var solver = new Day03B();
+        var solver = new Day03();
         solver.Part2("/Users/davidbetteridge/Personal/AdventOfCode2023/AOC_2023/AOC_2023.Tests/Day03/input.txt");
     }
 }
